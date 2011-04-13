130ed55fcf27675c5cc23bed5c70c4b162105f3d
Rizky Syazuli <br4inwash3r@gmail.com>
2011-04-13 22:33:42 +0700

    update the extension manifest with mobile boilerplate project structure

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -5,33 +5,32 @@ stylesheet 'partials/_boilerplate-defaults.scss'
 
 html 'index.html'
 
-javascript 'javascripts/libs/dd_belatedpng.js', :to => 'libs/dd_belatedpng.js'
-javascript 'javascripts/libs/jquery-1.5.1.js', :to => 'libs/jquery-1.5.1.js'
-javascript 'javascripts/libs/jquery-1.5.1.min.js', :to => 'libs/jquery-1.5.1.min.js'
-javascript 'javascripts/libs/modernizr-1.7.min.js', :to => 'libs/modernizr-1.7.min.js'
+javascript 'javascripts/libs/jquery-1.5.2.js', :to => 'libs/jquery-1.5.2.js'
+javascript 'javascripts/libs/jquery-1.5.2.min.js', :to => 'libs/jquery-1.5.2.min.js'
+javascript 'javascripts/libs/modernizr-custom.js', :to => 'libs/modernizr-custom.js'
+javascript 'javascripts/mylibs/helper.js', :to => 'mylibs/helper.js'
 javascript 'javascripts/plugins.js', :to => 'plugins.js'
 javascript 'javascripts/script.js', :to => 'script.js'
-# placeholder file for empty "mylibs" folder
-javascript 'files/gitignore_placeholder.txt', :to => 'mylibs/.gitignore'
 
 html 'files/404.html', :to => '404.html'
 html 'files/htaccess', :to => '.htaccess'
 html 'files/crossdomain.xml', :to => 'crossdomain.xml'
+html 'files/sitemap.xml', :to => 'sitemap.xml'
+file 'files/default.appcache', :to => 'default.appcache'
 file 'files/robots.txt', :to => 'robots.txt'
 file 'files/humans.txt', :to => 'humans.txt'
-html 'files/apple-touch-icon-57x57-precomposed.png', :to => 'apple-touch-icon-57x57-precomposed.png'
-html 'files/apple-touch-icon-72x72-precomposed.png', :to => 'apple-touch-icon-72x72-precomposed.png'
-html 'files/apple-touch-icon-114x114-precomposed.png', :to => 'apple-touch-icon-114x114-precomposed.png'
-html 'files/apple-touch-icon-precomposed.png', :to => 'apple-touch-icon-precomposed.png'
-html 'files/apple-touch-icon.png', :to => 'apple-touch-icon.png'
-html 'files/favicon.ico', :to => 'favicon.ico'
 file 'files/gitignore.txt', :to => '.gitignore'
-# placeholder file for empty "img" folder
-image 'files/gitignore_placeholder.txt', :to => '.gitignore'
 
-# html5-boilerplate build script
+# default images
+image 'images/h/apple-touch-icon.png',              :to => 'h/apple-touch-icon.png'
+image 'images/h/splash.png',                        :to => 'h/splash.png'
+image 'images/l/apple-touch-icon-precomposed.png',  :to => 'l/apple-touch-icon-precomposed.png'
+image 'images/l/apple-touch-icon.png',              :to => 'l/apple-touch-icon.png'
+image 'images/l/splash.png',                        :to => 'l/splash.png'
+image 'images/m/apple-touch-icon.png',              :to => 'm/apple-touch-icon.png'
+
+# build script
 file 'files/build/build.xml', :to => 'build/build.xml'
-file 'files/build/buildinfo.properties', :to => 'build/buildinfo.properties'
 file 'files/build/createproject.sh', :to => 'build/createproject.sh'
 file 'files/build/runbuildscript.bat', :to => 'build/runbuildscript.bat'
 # build configs
@@ -47,17 +46,17 @@ file 'files/build/tools/optipng-0.6.4-exe/LICENSE.txt', :to => 'build/tools/opti
 file 'files/build/tools/optipng-0.6.4-exe/optipng.exe', :to => 'build/tools/optipng-0.6.4-exe/optipng.exe'
 
 help %Q{
-This is a Compass extention for HTML5 Boilerplate by Paul Irish
-(See full docs at: http://github.com/sporkd/compass-html5-boilerplate)
+This is a Compass extention for Mobile HTML5 Boilerplate by Shi Chuan
+(See full docs at: https://github.com/rizkysyazuli/compass-html5-starter/tree/mobile)
 
 Installation
 ========================
-$ gem install html5-boilerplate
-$ compass create my_project -r html5-boilerplate -u html5-boilerplate --javascripts-dir assets/js --css-dir assets/css --images-dir assets/img --sass-dir assets/sass
+$ gem install html5-starter-mobile
+$ compass create my_project -r html5-boilerplate-mobile -u html5-boilerplate-mobile --javascripts-dir assets/js --css-dir assets/css --images-dir assets/img --sass-dir assets/sass
 
 }
 
 welcome_message %Q{
-You've installed HTML5 Boilerplate. Good for you!
+You've installed Mobile HTML5 Boilerplate. Good for you!
 
 }
